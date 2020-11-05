e77dc7ec552b7956c1c191a5e6de046bc4f8b954
Lowari <66225181+Lowari@users.noreply.github.com>
2020-11-05 08:36:15 +0100

    Simplification de code js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ var button = document.querySelector("#button-pass");
 var pass = document.querySelector("#pass");
 var confirm = document.querySelector("#confirmPass");
 
-button.addEventListener('click', function(e) {
-    let value = e.target.value;
+button.addEventListener('click', function() {
     if (pass.value == confirm.value) {
         pass.setAttribute('class', 'correct');
         confirm.setAttribute('class', 'correct');
